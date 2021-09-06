c5531f06bf01ce40729e952d5af594cd8757f75c
Josué Rubio <josu_mr@hotmail.com>
2021-09-06 16:31:40 +0200

    Starting a MERN app

---
+++ models/Item.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  image: {
+    type: String,
+    index: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+
+  /*
+    tags: [
+      {
+        type: String
+        enum: ['Armadura', 'Arma', 'Equipo Aventurero', 'Herramienta', 'Montura', 'Arreo', 'Vehículo Acuático', 'Mercancía', 'Alimentación', 'Servicio', 'Baratija'],
+        index: true,
+      },
+    ],
+  */
+  category: {
+    type: String,
+    required: true,
+    index: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+  },
+  date_added: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Item = mongoose.model('item', ItemSchema);
