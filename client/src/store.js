20ff04d99b7ef1343e06878ec2a9bb9a26204e68
Josué Rubio <josu_mr@hotmail.com>
2021-09-18 00:55:50 +0200

    composeEnhancers

--- client/src/store.js
+++ client/src/store.js
@@ -6,15 +6,29 @@ const initialState = {};
 
 const middleWare = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : () => {}
-  )
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleWare),
+//     window.__REDUX_DEVTOOLS_EXTENSION__
+//       ? window.__REDUX_DEVTOOLS_EXTENSION__()
+//       : () => {}
+//   )
+// );
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware)
+  // other store enhancers if any
 );
+
+const store = createStore(reducer, enhancer);
 
 export default store;
